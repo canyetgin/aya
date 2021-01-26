eda22080c8b99205e3db41024720426e0022737a
canyetgin <cannyetgin@gmail.com>
2021-01-26 20:53:19 +0300

    screen improvment

--- aya.py
+++ aya.py
@@ -1,11 +1,15 @@
 
 from time import sleep
 import sys
+
 import time
-for i in range(100):
+
+
+
+for i in range(50):
     time.sleep(0.1)
-    print(("[%d"%i+"%]")+("["+('#'*i)),end= ((99-i)*' ')+"] \r")
-    if(i==99):
+    print(("[%d"%i+"%]")+("["+('#'*i)),end= ((50-i)*' ')+"] \r")
+    if(i==49):
      time.sleep(0.1)
-     print(("[OK!]"),end=(100)*' '+" \r")
+     print(("[OK!]"),end=(51)*' '+" \r")
     
