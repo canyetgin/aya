c8cf853a4f9cb7cd3458051e3820a6c8f44830c2
canyetgin <cannyetgin@gmail.com>
2021-01-28 16:25:41 +0300

    improvment func

--- aya.py
+++ aya.py
@@ -1,21 +1,29 @@
 import requests
-import sys
-link = "https://download-cf.jetbrains.com/python/pycharm-community-2020.3.3.exe"
-file_name = "download.data"
-with open(file_name, "wb") as f:
-    print ("Downloading %s" % file_name)
-    response = requests.get(link, stream=True)
-    total_length = response.headers.get('content-length')
 
-    if total_length is None: # no content length header
+link = "https://images-na.ssl-images-amazon.com/images/I/71CY05k-CxL._AC_SL1500_.jpg"
+file_name = "download.data"
+
+
+def  downloadfile (file_name,link):
+      with open(file_name, "wb") as f:
+
+       response = requests.get(link, stream=True)
+       total_length = response.headers.get('content-length')
+
+       if total_length is None: # no content length header
         f.write(response.content)
-    else:
+       else:
         dl = 0
         total_length = int(total_length)
+
         for data in response.iter_content(chunk_size=4096):
             dl += len(data)
             f.write(data)
             done = int(50 * dl / total_length)
-            print("[%s%s]" % ('=' * done, ' ' * (50-done)), end="\r" )    
+
+            print("%s :[%s%s][%s%s]" % (  file_name,"%",done,'#' * done, ' ' * (50-done)), end="\r" )
            
-        print("[OK]",end=49*" "+"\r")    
+        print("[OK] "+file_name,end=55*" "+"\n")
+
+downloadfile ("download.data","https://images-na.ssl-images-amazon.com/images/I/71CY05k-CxL._AC_SL1500_.jpg")
+downloadfile ("download2.data","https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png")
