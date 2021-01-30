10d39a8abb54c32b96d3343a177e2eb8ded86dfd
canyetgin <cannyetgin@gmail.com>
2021-01-30 23:06:38 +0300

    bilmem

--- aya.py
+++ aya.py
@@ -1,8 +1,19 @@
 
 import requests
-link = "https://images-na.ssl-images-amazon.com/images/I/71CY05k-CxL._AC_SL1500_.jpg"
-file_name = "download.data"
 
+from win32api import GetFileVersionInfo, LOWORD, HIWORD
+def get_version_number(filename):
+    try:
+        info = GetFileVersionInfo (filename, "\\")
+        ms = info['FileVersionMS']
+        ls = info['FileVersionLS']
+        return HIWORD (ms), LOWORD (ms), HIWORD (ls), LOWORD (ls)
+    except:
+        return "Unknown version"
+
+version = ".".join([str (i) for i in get_version_number (
+        r'download.data')])
+print (version)
 
 def  downloadfile (file_name,link):
       with open(file_name, "wb") as f:
@@ -10,7 +21,7 @@ def  downloadfile (file_name,link):
        response = requests.get(link, stream=True)
        total_length = response.headers.get('content-length')
 
-       if total_length is None: # no content length header
+       if total_length is None: # no content l bvngth header
         f.write(response.content)
        else:
         dl = 0
@@ -26,4 +37,4 @@ def  downloadfile (file_name,link):
         print("[OK] "+file_name,end=55*" "+"\n")
 
 downloadfile ("download.data","https://images-na.ssl-images-amazon.com/images/I/71CY05k-CxL._AC_SL1500_.jpg")
-downloadfile ("download2.data","https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png")
+downloadfile ("download2.data","http://www.prolific.com.tw/UserFiles/files/PL2303_Prolific_DriverInstaller_v1200.zip")
